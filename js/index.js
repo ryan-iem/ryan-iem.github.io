c9e603c9864f16c809c67b5cab8c2f074e683535
Ryan Iem <ryan.iem12@gmail.com>
2026-02-07 17:29:31 +0900

    updates to literally everything

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ function changeTopic(selection) {
     }
     document.getElementById("profile-topic-" + selection).style = "text-decoration: underline; cursor: pointer;";
     document.getElementById("profile-topic-content-" + selection).style = "display: ''";
+    // document.getElementById("profile-topic-content-" + selection).style.animation = "spin2 100s linear infinite";
+    document.getElementById("profile-topic-content-" + selection).style.animation = "fadeIn 1.5s";
 }
 
 changeTopic(1);
