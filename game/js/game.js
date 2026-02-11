354094b7aa110830153b977a98cc6a1bbe560d3d
Ryan Iem <ryan.iem12@gmail.com>
2026-02-12 00:08:02 +0900

    adding onto combo system

--- game/js/game.js
+++ game/js/game.js
@@ -15,36 +15,134 @@ document.getElementById('player-def').innerHTML = player.getDef();
 document.getElementById('enemy-hp').innerHTML = enemy.getHp();
 // document.getElementById('player-gold').innerHTML = player.getGold();
 
-// Simulate keyboard combo feature (battle feature)
+// COMBO CODES ---
 keyCount = 0;
-function simulateComboSystem() {
-    const combo1 = ["ArrowUp", "ArrowRight", " "]; // Slash
-    const combo2 = ["ArrowUp", "ArrowRight", "ArrowRight", " "]; // Double slash (costs SP)
-    const combo3 = ["ArrowUp", "ArrowUp", "ArrowRight", " " ]; // Heavy slash (costs SP)
-    keyCountLimit = combo1.length;
+const combo1 = ["ArrowUp", "ArrowRight", " ", "4"]; // Slash
+const combo2 = ["ArrowUp", "ArrowRight", "ArrowRight", " ", "5"]; // Double slash (costs SP)
+const combo3 = ["ArrowUp", "ArrowUp", "ArrowRight", "ArrowRight", " ", "6"]; // Heavy slash (costs SP)
 
-    console.log("Combo Tracking ON");
-    document.getElementById('combo-btn').style = "background-color: red";
-    
-    function comboChecker() {
+// Simulate keyboard combo feature (for testComboSystem)
+function comboChecker() {
         console.log(event.key);
         if (event.key == combo1[keyCount]) {
             keyCount++;
+            key = event.key;
+
+            if (key == "ArrowUp") {
+                key = "↑";
+            } else if (key == "ArrowDown") {
+                key = "↓";
+            } else if (key == "ArrowLeft") {
+                key = "←";
+            } else if (key == "ArrowRight") {
+                key = "→";
+            } else if (key == " ") {
+                key = "Space";
+            }
+
+            document.getElementById('combo-display').innerHTML += key + " ";
             if (keyCount == keyCountLimit) {
                 console.log("Combo Executed!")
                 keyCount = 0;
                 document.removeEventListener('keydown', comboChecker);
-                document.getElementById('combo-btn').style = "background-color: green";
             }
         } else {
             console.log("Incorrect key pressed. Combo Failed!")
             keyCount = 0;
             document.removeEventListener('keydown', comboChecker);
-            document.getElementById('combo-btn').style = "background-color: green";
+            document.getElementById('combo-display').innerHTML = "&nbsp;";
         }
-    }
-    document.addEventListener('keydown', comboChecker);  
 }
+function testComboSystem() {
+
+    keyCountLimit = combo1.length - 1;  // The last value is the DMG value
+
+    console.log("Combo Tracking ON");
+    document.getElementById('combo-display').innerHTML = "&nbsp;";
+    document.addEventListener('keydown', comboChecker);
+}
+
+// Simulate keyboard combo feature (for simulateComboAttackFromPlayer)
+function simulateComboAttack() {
+        console.log(event.key);
+        if (event.key == combo1[keyCount]) {
+            keyCount++;
+            key = event.key;
+
+            if (key == "ArrowUp") {
+                key = "↑";
+            } else if (key == "ArrowDown") {
+                key = "↓";
+            } else if (key == "ArrowLeft") {
+                key = "←";
+            } else if (key == "ArrowRight") {
+                key = "→";
+            } else if (key == " ") {
+                key = "Space";
+            }
+
+            document.getElementById('combo-display').innerHTML += key + " ";
+            if (keyCount == keyCountLimit) {
+                console.log("Combo Executed!")
+                keyCount = 0;
+                document.removeEventListener('keydown', comboChecker);
+            }
+        } else {
+            console.log("Incorrect key pressed. Combo Failed!")
+            keyCount = 0;
+            document.removeEventListener('keydown', comboChecker);
+            document.getElementById('combo-display').innerHTML = "&nbsp;";
+        }
+}
+function simulateComboAttackFromPlayer() {
+    keyCountLimit = combo1.length - 1; // The last value is the DMG value
+    console.log("Combo Tracking ON");
+    document.getElementById('combo-display').innerHTML = "&nbsp;";
+    document.addEventListener('keydown', simulateComboAttack);
+}
+// COMBO CODES ---
+
+// Simulate an attack (Player -> enemy)
+// TODO: Change attack() to take a value and not an object
+// function simulateComboAttackFromPlayer() {
+
+//     if (enemy.getHp() > 0 && player.getHp() > 0) {
+//         chance = Math.random();
+//         if (chance > 0.33) { // 33% chance to miss and 66% chance to hit
+
+//             player.attack(enemy);
+//             // alert("The enemy has " + enemy.getHp() + " HP remaining!")
+
+//             if (enemy.getHp() > 0 && player.getHp() > 0) {
+
+//                 alert("The enemy takes " + player.getAtk() + " damage!")
+//                 alert("The enemy has " + enemy.getHp() + " HP remaining!")
+
+//             } else {
+//                 alert("The enemy takes " + player.getAtk() + " damage!")
+//                 alert("You have slain the enemy!")
+//                 // TODO: Logic to remove enemy? Rewards?
+//             }
+//         } else {
+//             alert("Your attack misses!")
+//         }
+
+//     } else {
+//         if (enemy.getHp() == 0) {
+//             alert("The enemy is already dead!")
+//         } else {
+//             alert("You are dead!")
+//         }
+//     }
+
+//     // player.attack(enemy);
+//     // alert("The enemy has " + enemy.getHp() + " HP remaining!")
+//     // player.attack(enemy);
+//     // alert("The enemy has " + enemy.getHp() + " HP remaining!")
+//     document.getElementById('player-hp').innerHTML = player.getHp();
+//     document.getElementById('enemy-hp').innerHTML = enemy.getHp();
+
+// }
 
 // Simulate an attack (Player -> enemy)
 function simulateAttackFromPlayer() {
